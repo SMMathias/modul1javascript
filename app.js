357fe932fa4147a0555ec9fce7f607377c7f6483
SMMathias <mathias08102004@gmail.com>
2025-09-24 12:49:24 +0200

    tak vro

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-function sayHello() {
-}
-
+function sayHello() {}
 
 function sayHello() {
-	document.querySelector("h3").innerHTML = "Hej " + document.querySelector("#name").value + "<br>" + "Du er " + document.querySelector("#age").value + " år gammel"
-    + "<br>" + "Din email er " + document.querySelector("#email").value;
-   
+  document.querySelector("h3").innerHTML =
+    "Hej " +
+    document.querySelector("#name").value +
+    "<br>" +
+    "Du er " +
+    document.querySelector("#age").value +
+    " år gammel" +
+    "<br>" +
+    "Din email er " +
+    document.querySelector("#email").value;
 }
 
-
-document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("#btn").addEventListener("click", sayHello);
